86d1e554ac720e4a862dac7e51dc3c54b13699a7
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-07-30 10:53:28 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
-window.onscroll=()=>{
+/*========= Sticky navbar ============*/
+window.onscroll = () =>{
 let header = document.querySelector('.header');
-
-header.classList.toggle('sticky',window.scrolly > 100);
+header.classList.toggle('sticky', window.scrollY > 100);
 };
