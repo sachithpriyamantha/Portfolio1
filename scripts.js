9ad7e2a5ee3dcda40154c7f77e044da990dd56c4
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-08-02 21:49:15 +0530

    update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
-/*===========Sticky navbar==========*/
+/*======================Sticky navbar============================*/
 window.onscroll = () => {
     let header = document.querySelector('.header');
     
     header.classList.toggle('sticky', window.scrollY > 100);
     };
+
+/*======================Swiper============================*/
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    grabCursor:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
